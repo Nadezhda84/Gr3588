33be79b41b84212adfb6effc1f9db29ebf14b0fb
Dusha <nadezdafurman84@gmail.com>
2022-12-03 01:13:37 +0600

    added task 16

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+void Test(int number1, int number2)
+{
+    bool result = (number1 / number2 == number2);
+    
+    if (result) Console.WriteLine("Число " + number1 + " является квадратом числа " + number2);
+    
+    else Console.WriteLine("Число " + number1 + " не является квадратом числа " + number2);
+    
+}
+
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+Test(num1,num2);
+Test(num2,num1);
