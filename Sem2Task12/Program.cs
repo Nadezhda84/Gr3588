5b79de6b46a57772a5373cfbc3b61150f12da941
Dusha <nadezdafurman84@gmail.com>
2022-12-03 01:12:21 +0600

    Added task 12

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = int.Parse(Console.ReadLine() ?? "0");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+int remainderOfDiv = num1 % num2;
+if (remainderOfDiv == 0)
+{
+    Console.WriteLine(num1 + " делится на " + num2);
+}
+else
+{
+    Console.Write(num1 + " не делится на " + num2 + ". Остаток от деления: " + remainderOfDiv);
+}
