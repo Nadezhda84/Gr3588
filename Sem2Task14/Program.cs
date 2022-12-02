2cb1a66fe36e186e4d704c78e0784884988c9297
Dusha <nadezdafurman84@gmail.com>
2022-12-03 01:12:52 +0600

    added task 14

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = int.Parse(Console.ReadLine() ?? "0");
+
+// int result1 = num % 7;
+// int result2 = num % 23;
+
+
+if ((num % 7 == 0) && (num % 23 == 0))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
