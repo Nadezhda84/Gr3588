50326bdd9d6d73da88677a7995fd396bf1f8f614
Dusha <nadezdafurman84@gmail.com>
2022-12-03 01:11:42 +0600

    added task 11

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+//Получаем новое случайное число
+int number = numberSintezator.Next(100, 1000);
+
+//Выводим данные в консоль
+Console.WriteLine(number);
+
+//Получаем первую цифру и вторую цифру поотдельности
+// int firstNumber = number / 100;
+// int secondNumber = number % 10;
+
+
+
+Console.WriteLine((number/100)*10 + number%10);
