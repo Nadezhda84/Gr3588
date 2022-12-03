b00417b50c831cc37bb9231a4624e49232582f25
Dusha <nadezdafurman84@gmail.com>
2022-12-03 22:49:56 +0600

    Added Task10

---
+++ Homework2Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number > 99 && number < 1000)                                           //Проверяем, что число трехзначное
+    Console.WriteLine((number / 10) % 10);                                  //Выводим на печать вторую цифру числа
+else Console.WriteLine("Вы ввели не трехзначное число.");
