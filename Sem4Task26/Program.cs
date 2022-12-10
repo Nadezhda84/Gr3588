54b3ec4f613929d842fa27f26c8b692311d27caa
Dusha <nadezdafurman84@gmail.com>
2022-12-10 19:27:28 +0600

    Added Task 26

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,33 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int VariantLog(int num)
+{
+    int count = (int)Math.Log10(num)+1;
+    return count;
+}
+
+int VariantSimple(int num)
+{
+    int count = 0;
+    for (int i=0; i<num; i++)
+    {
+        num=num/10;
+        count++;
+    }
+    return count;
+}
+
+int number = ReadData("Введите число А:");
+int length1 = VariantLog(number);
+int length2 = VariantSimple(number);
+PrintData("Количество цифр в числе равно: " + length1);
+PrintData("Количество цифр в числе равно: " + length2);
