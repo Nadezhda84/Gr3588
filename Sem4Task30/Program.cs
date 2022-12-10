de8f58ff0d4613ef36048dec8833b7f22a5a60b5
Dusha <nadezdafurman84@gmail.com>
2022-12-10 19:28:24 +0600

    Added Task 30

---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,36 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res, int[] arr)
+{
+    Console.WriteLine(res);
+    PrintArray(arr);
+}
+
+int[] GenArray(int len)
+{
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i=0; i <arr.Length;i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i=0; i<array.Length-1; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+    Console.WriteLine(array[array.Length-1] + "]");
+}
+
+int number = ReadData("Введите количество элементов в массиве: ");
+int[] array = GenArray(number);
+PrintData("Массив ", array);
