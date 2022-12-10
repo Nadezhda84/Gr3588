543df7cabf5f234ac3d4c7256d959af484a174bc
Dusha <nadezdafurman84@gmail.com>
2022-12-10 19:27:54 +0600

    Added Task 28

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+BigInteger Factorial(int num)
+{
+    BigInteger factorial = 1;
+    for (int i=1; i<= num; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int number = ReadData("Введите число: ");
+BigInteger fact = Factorial(number);
+PrintData("Факториал числа равен: " + fact);
