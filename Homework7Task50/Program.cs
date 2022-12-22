5cf9c9565e0833e29cc78498046a4a23959f9822
Dusha <nadezdafurman84@gmail.com>
2022-12-22 13:03:00 +0600

    Added Task50

---
+++ Homework7Task50/Program.cs
@@ -0,0 +1,69 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// * Заполнить числами Фиббоначи и выделить цветом найденную цифру
+
+//Метод, считывающий данные, введенные пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+// Генерируем двумерный массив, заполненный случайными числами
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+// Вывод цветного массива на печать
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(matrix[i, j] + " ");
+            Console.ResetColor();                                                   // Устанавливает исходный цвет
+        }
+        Console.WriteLine();
+    }
+}
+
+int SearchElem(int[,] arr, int rowElem, int colElem)
+{
+    int res = -1;
+    if (rowElem < arr.GetLength(0) && colElem < arr.GetLength(1)) res = arr[rowElem, colElem];
+    return res;
+}
+
+int row = ReadData("Введите количество строк");
+int column = ReadData("Введите количество столбцов");
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
+Print2DArrayColor(arr2D);
+
+int currentRow = ReadData("Введите номер строки");
+int currentCol = ReadData("Введите номер столбца");
+int elem = SearchElem(arr2D,currentRow, currentCol);
+if (elem==-1) PrintData("Такого числа в массиве нет");
+else PrintData($"На позиции [{currentRow},{currentCol}] находится число {elem}");
