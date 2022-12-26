d1666b5e30ae0b96a7940542a752d4783d6305ab
Dusha <nadezdafurman84@gmail.com>
2022-12-26 09:16:38 +0600

    Added Task67

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,24 @@
+﻿// примает на вход число и возвращает сумму цифр числа
+
+// Чтение данных из консоли
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Печать результата
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int RecSumDigit(int N)
+{
+    if (N < 10) return N;
+    return N % 10 + RecSumDigit(N / 10);
+}
+
+int number = ReadData("Введите число N: ");
+int sumDigit = RecSumDigit(number);
+PrintData("Сумма цифр числа равна: " + sumDigit);
