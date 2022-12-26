b41b3f0a9531e7c151e8390222434dc9ef4186e8
Dusha <nadezdafurman84@gmail.com>
2022-12-26 09:16:28 +0600

    Added Task65

---
+++ Sem9Task65/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт все натуральные элементы в промежутке от M до N.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Печать результата
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+string RecMN(int M, int N)
+{
+    string outRes = string.Empty;
+    if (M >= N) return outRes + N;
+    outRes = M + " " + RecMN(M + 1, N);
+    return outRes;
+}
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+string resultLine = number1 < number2 ? RecMN(number1, number2) : RecMN(number2, number1); // Тернарный оператор(если пользователь перепутал M и N)
+PrintData(resultLine);
